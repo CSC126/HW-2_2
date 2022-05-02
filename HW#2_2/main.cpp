1a8d835225611773efc3e31f9ba897eec287d28d
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-05-01 21:06:39 -0400

    Initial Commit

---
+++ HW#2_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW#2_2
+//
+//  Created by Maxim Voyevoda on 5/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
